97c9708588fd3f55bffd99fee251131ff9444bf9
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-23 14:02:45 -0500

    Added useDispatch in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
 
 function App() {
-
-  // Order checkout
-
-
-  // Order Enter Customer Info
-
-
-  // Order Select Pizza
-
-  
-  // Order Admin
-
-
-  // Order Display all pizzas
-
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-      
+  
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
