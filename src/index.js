3d9e30f6bddb6d43ea16786b4eb9b9ed92a0138d
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-23 13:24:28 -0500

    Added Provider and combineReducers from redux

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+
+
+// Plop in the reducers you want shared through your store here
+const storeInstance = createStore(
+    combineReducers({
+        pizzaReducer,
+    })
+)
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
