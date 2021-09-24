1c595af1f4ad2e29b53cde7f883c66dd120b42cf
XGalilei <ianbush1@gmail.com>
2021-09-24 10:05:13 -0500

    Adding in additional formatting to prepare for final step

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import CustomerForm from '../CustomerForm/CustomerForm';
+import {useDispatch} from 'react-redux';
 
 function App() {
 
--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import {useHistory} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+//import {useHistory} from 'react-router-dom';
 
-function CustomerForm() {   
+function CustomerForm() {
+
+    const reduxStore = useSelector(store => store);
 
     //will need to retrieve the customer's order first
     const [name, setName] = useState('');
@@ -10,6 +13,13 @@ function CustomerForm() {
     const [zip, setZip] = useState('');
     //const [type, setType] = useState('');
 
+    const moveObject = (object) => {
+        const action = {
+            type: 'MOVE_OBJECT_UP',
+            payload: object
+        }
+    };
+
     const handleNext = (event) => {
         event.preventDefault();
         //done to prevent code from running automatically
@@ -17,8 +27,8 @@ function CustomerForm() {
         // TO-DO: Add-in code to handle next step
         // will need to add to the existing order object, 
         const orderObject = {
-            name: name,
-            address: address,
+            customer_name: name,
+            street_address: address,
             city: city,
             zip: zip,
             type: 'delivery'
@@ -32,36 +42,36 @@ function CustomerForm() {
     return <div>
         <form onSubmit={handleNext}>
             <div>
-            <input 
-            onChange={(event) => setName(event.target.value)}
-            value={name}
-            required
-            placeholder="Name" /> <br/>
-            <input
-            onChange={(event) => setAddress(event.target.value)}
-            value={address}
-            required 
-            placeholder="Street Adresss" /> <br/>
-            <input 
-            onChange={(event) => setCity(event.target.value)}
-            value={city}
-            required
-            placeholder="City"/> <br/>
-            <input 
-            onChange={(event) => setZip(event.target.value)}
-            value={zip}
-            required
-            placeholder="Zip"/> <br/>
+                <input
+                    onChange={(event) => setName(event.target.value)}
+                    value={name}
+                    required
+                    placeholder="Name" /> <br />
+                <input
+                    onChange={(event) => setAddress(event.target.value)}
+                    value={address}
+                    required
+                    placeholder="Street Adresss" /> <br />
+                <input
+                    onChange={(event) => setCity(event.target.value)}
+                    value={city}
+                    required
+                    placeholder="City" /> <br />
+                <input
+                    onChange={(event) => setZip(event.target.value)}
+                    value={zip}
+                    required
+                    placeholder="Zip" /> <br />
             </div>
 
             <div>
-            <input type="radio" id="pickup" name= "type" value="pickup"/>
-            <label htmlFor="pickup">Pickup</label>
-            <input type="radio" id="delivery" name="type" value="delivery"/>
-            <label htmlFor="delivery">Delivery</label>
+                <input type="radio" id="pickup" name="type" value="pickup" />
+                <label htmlFor="pickup">Pickup</label>
+                <input type="radio" id="delivery" name="type" value="delivery" />
+                <label htmlFor="delivery">Delivery</label>
             </div>
 
-            <input type="submit" value="Next"/>
+            <input type="submit" value="Next" />
         </form>
     </div>;
 }
